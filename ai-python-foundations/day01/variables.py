81c347eb93a8472438cd65d99ccba9a3c4abc403
B. Sai Madhavan <saimadhavan2006@gmail.com>
2026-09-07 15:17:20 +0530

    variables

---
+++ ai-python-foundations/day01/variables.py
@@ -0,0 +1,13 @@
+#dynamic typing
+name = "maddy"
+#int bool string float
+age = 20
+weight = 60.5
+is_student = True
+age = "20"
+grade = "A"
+print(type(age))
+print(type(weight))
+print(type(is_student))
+print(type(grade))  
+print(type(name))
