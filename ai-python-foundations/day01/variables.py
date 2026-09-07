60353d8a5fbb4cf528d05ae8f80fd564372e822a
saimadhavan2006-cloud <saimadhavan2006@gmail.com>
2026-09-07 15:28:36 +0530

    Add total variable and print its type
    Added a new variable 'total' and printed its type.

--- ai-python-foundations/day01/variables.py
+++ ai-python-foundations/day01/variables.py
@@ -6,8 +6,10 @@ weight = 60.5
 is_student = True
 age = "20"
 grade = "A"
+total = "billa"
 print(type(age))
 print(type(weight))
 print(type(is_student))
 print(type(grade))  
 print(type(name))
+print(total)
